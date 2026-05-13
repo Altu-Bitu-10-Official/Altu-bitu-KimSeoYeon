464c449f8157f8894ddac9515d039b0769a72c7c
seoyeon <2371013@ewhain.net>
2026-05-13 17:38:08 +0900

    [DFS] 0513

---
+++ src/_07_dfs/_11726.java
@@ -0,0 +1,19 @@
+package _07_dfs;
+import java.util.Scanner;
+
+public class _11726 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] d = new int[1001];
+        d[1] = 1;
+        d[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            d[i] = (d[i-1] + d[i-2]) % 10007;
+        }
+
+        System.out.println(d[n]);
+    }
+}
